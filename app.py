7684281ece7069414b02e5477940d73b0ff05807
Guangsheng <w.max@foxmail.com>
2019-10-11 15:38:18 +0800

    Revert "add: add aaa api."
    This reverts commit e8f756b1eded5db458c654996e462ff1a0d3605f.

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 
 from conf.config import HOST, PORT, CORS_RESOURCES
-from blueprint.olt_bp import bp as obp
-from blueprint.aaa_bp import bp as abp
+from blueprint.olt_bp import bp
 
 app = Flask(__name__)
 CORS(app, resources=CORS_RESOURCES)
 
-app.register_blueprint(obp)
-app.register_blueprint(abp)
+app.register_blueprint(bp)
 
 
 if __name__ == '__main__':
