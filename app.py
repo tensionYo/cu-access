46031fc7620d6ddaa82a75aa14095e8c49e45a2f
Guangsheng <w.max@foxmail.com>
2019-10-11 15:28:34 +0800

    add: add aaa api.

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 
 from conf.config import HOST, PORT, CORS_RESOURCES
-from blueprint.olt_bp import bp
+from blueprint.olt_bp import bp as obp
+from blueprint.aaa_bp import bp as abp
 
 app = Flask(__name__)
 CORS(app, resources=CORS_RESOURCES)
 
-app.register_blueprint(bp)
+app.register_blueprint(obp)
+app.register_blueprint(abp)
 
 
 if __name__ == '__main__':
--- utils/tools.py
+++ utils/tools.py
@@ -6,3 +6,37 @@ def all_none(params):
         if i:
             return False
     return True
+
+
+def parse_traffic_rate(s):
+    """
+    :type s: str
+    :param s:
+    :return:
+    """
+    if not s:
+        return 0
+    if s.endswith('Mb/s'):
+        return int(float(s[:len(s) - 4]))
+    else:
+        return -1
+
+
+def check_eval_matrix(m):
+    """
+    :type m: list[list[flost]]
+    :param m:
+    :return:
+    """
+    try:
+        fm = [map(float, r) for r in m]
+        rn, cn = len(m), len(m[0])
+        if rn != cn:
+            return None, False
+        for r in range(rn, rn):
+            for c in range(cn):
+                if abs(fm[r][c] * fm[c][r] - 1) > 0.0001:
+                    return None, False
+        return fm, True
+    except Exception as e:
+        return None, False
