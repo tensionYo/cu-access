939c9b19a2d254ced264fddbc29f1f8e226d8845
Guangsheng <w.max@foxmail.com>
2019-10-30 22:52:26 +0800

    add: add admin_role restriction to admin api.
    ATTENTION: @admin_role must be placed after @json_resp.

--- blueprint/product_bp.py
+++ blueprint/product_bp.py
@@ -1,7 +1,7 @@
 # encoding = utf-8
 from flask import Blueprint, request
 from flask_login import login_required
-from utils.decorator import json_resp
+from utils.decorator import json_resp, admin_role
 from blueprint.product_service import *
 
 bp = Blueprint('product_bp', __name__, url_prefix='/product')
@@ -12,6 +12,7 @@ PRODUCT_KEYS = []
 @bp.route('/create/')
 @login_required
 @json_resp
+@admin_role
 def create_product():
     params = {}
     for k in PRODUCT_KEYS:
@@ -23,5 +24,6 @@ def create_product():
 @bp.route('/save/')
 @login_required
 @json_resp
+@admin_role
 def modify_product():
     return None
