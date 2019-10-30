8a3ee5fbc036ff332081df63bfd997d26388dc74
Guangsheng <w.max@foxmail.com>
2019-10-30 23:02:40 +0800

    add: add /create/ demo.

--- blueprint/product_bp.py
+++ blueprint/product_bp.py
@@ -6,7 +6,8 @@ from blueprint.product_service import *
 
 bp = Blueprint('product_bp', __name__, url_prefix='/product')
 
-PRODUCT_KEYS = []
+DEFAULT_EMPTY = '--EMPTY--'
+PRODUCT_KEYS = ['bandwidth', 'delay', 'packet_loss_rate']
 
 
 @bp.route('/create/')
@@ -14,9 +15,9 @@ PRODUCT_KEYS = []
 @json_resp
 @admin_role
 def create_product():
-    params = {}
+    params = []
     for k in PRODUCT_KEYS:
-        params[k] = request.args.get(k, '').encode('utf-8')
+        params.append(request.args.get(k, DEFAULT_EMPTY).encode('utf-8'))
     add_product(params)
     return dict(success=True, data='ok')
 
--- blueprint/product_service.py
+++ blueprint/product_service.py
@@ -4,9 +4,12 @@ from db.client import cli
 
 def add_product(params):
     """
-    :type params: dict
+    :type params: list
     :param params:
     :return:
     """
-    sql = ""
-    cli.execute()
+    # TODO fix sql and table
+    vs = tuple(params)
+    sql = "insert into xxx (bandwidth, delay, packet_loss_rate) " \
+          "VALUES '%s', '%s', '%s';" % vs
+    cli.execute(sql)
